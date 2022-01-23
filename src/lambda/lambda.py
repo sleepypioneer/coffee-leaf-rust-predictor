985a3618a9ce0eb4116dc8de46acc5787fde66cd
Jessica Greene <jessica0greene@gmail.com>
2022-01-23 14:32:53 +0100

    refactor lambda to take arguments

--- src/lambda/lambda.py
+++ src/lambda/lambda.py
@@ -1,31 +1,38 @@
+import os
+
 import tflite_runtime.interpreter as tflite
 from keras_image_helper import create_preprocessor
 
 input_size = 299
+model_name = os.getenv("MODEL_NAME")
 
-preprocessor = create_preprocessor('xception', target_size=(input_size, input_size))
+preprocessor = create_preprocessor("xception", target_size=(input_size, input_size))
 
-interpreter = tflite.Interpreter(model_path="./xception_v1_2022-01-16_16_30.tflite")
+model_path = f"./{model_name}"
+print(f"model path: {model_path}")
+
+interpreter = tflite.Interpreter(model_path=model_path)
 interpreter.allocate_tensors()
 
 
-classes = ['other', 'rust']
+classes = ["other", "rust"]
 
 input_index = interpreter.get_input_details()[0]["index"]
 output_index = interpreter.get_output_details()[0]["index"]
 
 
-def predict(img_url: str, input_size: int=input_size):
+def predict(img_url: str, input_size: int = input_size):
     X = preprocessor.from_url(img_url)
 
     interpreter.set_tensor(input_index, X)
     interpreter.invoke()
     preds = interpreter.get_tensor(output_index)
-    
-    return dict(zip(classes, str(preds[0])))
+
+    return dict(zip(classes, preds[0].astype(str)))
 
 
 def lambda_handler(event, context):
-    url = event['url']
+    url = event["url"]
     result = predict(url)
+    print(result)
     return result
